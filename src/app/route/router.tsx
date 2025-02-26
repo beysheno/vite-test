3665a4903c1b4d994a70af590e18bea5f19e986d
Aziret Beyshenov <beyshenov11@icloud.com>
2025-02-26 17:15:39 +0600

    init commit

---
+++ src/app/route/router.tsx
@@ -0,0 +1,17 @@
+import {createBrowserRouter} from "react-router-dom";
+import {Layout} from "../layout/Layout.tsx";
+import {route} from "../../shared/configs/constats/contants.ts";
+import {HomePage} from "../../pages/home/view/HomePage.tsx";
+
+export const router  = createBrowserRouter([
+    {
+        element: <Layout/>,
+        children:[
+            {
+                path: route.home,
+                element: <HomePage/>
+            }
+        ]
+
+    }
+])

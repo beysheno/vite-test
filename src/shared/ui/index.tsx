405b92c812e2dee9f399a83ba071a3f7dc069da3
Saykal <ryskulovasaykal10@gmail.com>
2025-03-03 04:43:48 +0600

    bugs fixed

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,15 +1,1 @@
-import { useState } from 'react'
-import { Button } from './button/button.tsx'
-import { Pagination } from './pagination/ui/Pagination.tsx'
 
-export const App = () => {
-    const [page, setPage] = useState(1);
-
-    return (
-        <div>
-            <h1>Pagination</h1>
-    <Button onClick={() => alert("Clicked!")} variant="primary">Primary Button</Button>
-    <Pagination totalPages={10} currentPage={page} onPageChange={setPage} />
-    </div>
-)
-}
